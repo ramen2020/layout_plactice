d43ccd77a7bbbe8634611490d1a080552e0a014e
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-12-16 09:41:40 +0900

    init

---
+++ layout_plactice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  layout_plactice
+//
+//  Created by 宮本光直 on 2021/12/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
